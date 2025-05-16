81a1398de2978f717b8cb4f6a2c6d5484d019cba
Joy-LJM <2522702541@qq.com>
2025-05-15 22:43:46 -0400

    implant react query provider

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>                                   
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
